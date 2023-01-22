4b5e79b8c0847736720ffb3361793c3d4f851385
Nikita Naumov <naumov99.rst@gmail.com>
2023-01-22 11:41:40 +0300

    feat: update temp

--- config/database.js
+++ config/database.js
@@ -1,10 +1,14 @@
 const path = require("path");
 
-module.exports = () => ({
+module.exports = ({ env }) => ({
   connection: {
     client: "sqlite",
     connection: {
-      filename: path.join(__dirname, "..", ".tmp/data.db"),
+      filename: path.join(
+        __dirname,
+        "..",
+        env("DATABASE_FILENAME", ".tmp/data.db")
+      ),
     },
     useNullAsDefault: true,
   },
